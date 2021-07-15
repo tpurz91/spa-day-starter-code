e4e61f6c12fb629ce0f830bba5578a6308b674b3
tpursl000 <Tim_Pursley@cable.comcast.com>
2021-07-15 18:04:30 -0400

    committed

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,8 +1,18 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class User {
-    private String username;
+    @NotBlank(message = "Must enter username")
+    @Size(min = 5, max = 15, message = "Must be between 5-15 characters")
+    private String username;   //****non-empty and 5-15 characters long.
+    @Email(message = "Not a valid email")
     private String email;
+    @NotBlank(message = "Password is required")
+    @Size(min=6, message = "Password must be a least 6 characters")
     private String password;
 
     public User() {
